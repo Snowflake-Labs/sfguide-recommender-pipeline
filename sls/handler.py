be73c293718cf6b2efad98885a30f28eb212827b
filanthropic <ahmad.khan@gmail.com>
2021-05-05 11:31:26 -0700

    fixed s3 bucket issue

--- sls/handler.py
+++ sls/handler.py
@@ -36,7 +36,7 @@ def train_and_generate_recommendations(event, context):
         # start the SageMaker training job
         client = boto3.client('sagemaker')
 
-        bucket = os.environ['s3_bucket']
+        bucket = os.environ['s3_bucket'] 
         prefix = "training-job-" + time.strftime("%Y%m%d%H%M%S")
 
         s3_output_location = 's3://{}/'.format(bucket)
